a4b193121d7d619f74307979914d2c4b16cf907b
Aman Singh <amanrawat6767@gmail.com>
2024-12-12 20:35:50 +0530

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:cloudinary_file_upload/views/home.dart';
 import 'package:cloudinary_file_upload/views/upload_area.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
--- lib/views/upload_area.dart
+++ lib/views/upload_area.dart
@@ -1,3 +1,4 @@
+import 'package:cloudinary_file_upload/services/cloudinary_service.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -10,13 +11,92 @@ class UploadArea extends StatefulWidget {
 }
 
 class _UploadAreaState extends State<UploadArea> {
+  late FilePickerResult _selectedFile;
+
+  void _uploadFile() async {
+    final result = await uploadToCloudinary(_selectedFile);
+
+    if(!context.mounted) {
+      return;
+    }
+
+    if (result) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("File Uploaded Successfully."),
+        ),
+      );
+      Navigator.pop(context);
+
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Cannot Upload Your File."),
+        ),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedFile =
+        ModalRoute.of(context)!.settings.arguments as FilePickerResult;
+  }
+
   @override
   Widget build(BuildContext context) {
-    final selectedFile =
-        ModalRoute.of(context)!.settings.arguments as FilePickerResult;
     return Scaffold(
       appBar: AppBar(
         title: Text("Upload Area"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextFormField(
+              readOnly: true,
+              initialValue: _selectedFile.files.first.name,
+              decoration: const InputDecoration(
+                label: Text("Name"),
+              ),
+            ),
+            TextFormField(
+              readOnly: true,
+              initialValue: _selectedFile.files.first.extension,
+              decoration: const InputDecoration(
+                label: Text("Extension"),
+              ),
+            ),
+            TextFormField(
+              readOnly: true,
+              initialValue: "${_selectedFile.files.first.size} bytes",
+              decoration: const InputDecoration(
+                label: Text("Size"),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text("Cancel"),
+                  ),
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: _uploadFile,
+                    child: Text("Upload"),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
